0f9d703ac8c840885a87af15ff39a12ed402990d
chenhai <chenhai@atguigu.com>
2022-11-24 10:01:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author chenhai
+ * @create 2022-11-23 20:48
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
